bac297f308786857cdb9ccd869a1ef6dbed65fa0
Himanshu Bhadra <59540185+bayareala8s@users.noreply.github.com>
2024-12-18 18:20:03 -0800

    Update logical.tf

--- SFTP/logical.tf
+++ SFTP/logical.tf
@@ -1,40 +1,49 @@
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::123456789012:role/sftp-access-role"
+resource "aws_iam_policy" "sftp_policy" {
+  name        = "sftp-access-policy"
+  description = "Policy to restrict user access to specific folders and prevent folder creation"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "s3:ListBucket",
+        Resource = "arn:aws:s3:::${var.bucket_name}",
+        Condition = {
+          StringLike: {
+            "s3:prefix": [
+              "user1/",
+              "user1/folder1/*",
+              "user1/folder2/*"
+            ]
+          }
+        }
       },
-      "Action": [
-        "s3:ListBucket"
-      ],
-      "Resource": "arn:aws:s3:::my-sftp-bucket",
-      "Condition": {
-        "StringLike": {
-          "s3:prefix": [
-            "user1/*",
-            "user1/folder1/*",
-            "user1/folder2/*"
-          ]
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}/user1/folder1/*",
+          "arn:aws:s3:::${var.bucket_name}/user1/folder2/*"
+        ]
+      },
+      {
+        Effect: "Deny",
+        Action: "s3:PutObject",
+        Resource: [
+          "arn:aws:s3:::${var.bucket_name}/user1/",
+          "arn:aws:s3:::${var.bucket_name}/user1/folder1/",
+          "arn:aws:s3:::${var.bucket_name}/user1/folder2/"
+        ],
+        Condition: {
+          StringEquals: {
+            "s3:x-amz-object-lock-mode": "COMPLIANCE"
+          }
         }
       }
-    },
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::123456789012:role/sftp-access-role"
-      },
-      "Action": [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject"
-      ],
-      "Resource": [
-        "arn:aws:s3:::my-sftp-bucket/user1/*",
-        "arn:aws:s3:::my-sftp-bucket/user1/folder1/*",
-        "arn:aws:s3:::my-sftp-bucket/user1/folder2/*"
-      ]
-    }
-  ]
+    ]
+  })
 }
