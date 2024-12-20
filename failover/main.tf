1b1c96a7e046337f4546db0bc0cd94ffa56677e9
Himanshu Bhadra <59540185+bayareala8s@users.noreply.github.com>
2024-12-20 14:34:39 -0800

    Update main.tf

--- failover/main.tf
+++ failover/main.tf
@@ -1,15 +1,22 @@
+# Provider for the primary region (us-west-1)
 provider "aws" {
   region = "us-west-1"
 }
 
-# Create Primary S3 Bucket
+# Provider for the replica region (us-east-1)
+provider "aws" {
+  alias  = "replica"
+  region = "us-east-1"
+}
+
+# Create Primary S3 Bucket in us-west-1
 resource "aws_s3_bucket" "primary_bucket" {
   bucket = var.primary_bucket_name
   versioning {
     enabled = true
   }
 
-  # Cross-region replication to the replica bucket
+  # Cross-region replication configuration
   replication_configuration {
     role = aws_iam_role.s3_replication_role.arn
 
@@ -33,10 +40,11 @@ resource "aws_s3_bucket" "primary_bucket" {
   }
 }
 
-# Create Replica S3 Bucket
+# Create Replica S3 Bucket in us-east-1
 resource "aws_s3_bucket" "replica_bucket" {
+  provider = aws.replica
+
   bucket = var.replica_bucket_name
-  region = "us-east-1"
   versioning {
     enabled = true
   }
@@ -46,7 +54,7 @@ resource "aws_s3_bucket" "replica_bucket" {
   }
 }
 
-# Create IAM Role for S3 Replication
+# IAM Role for S3 Replication
 resource "aws_iam_role" "s3_replication_role" {
   name = "s3-replication-role"
 
@@ -95,105 +103,4 @@ resource "aws_iam_role_policy" "s3_replication_policy" {
       }
     ]
   })
-}
-
-# Create IAM Role for Lambda Functions
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = { Service = "lambda.amazonaws.com" }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda-policy"
-  role = aws_iam_role.lambda_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.primary_bucket.arn,
-          "${aws_s3_bucket.primary_bucket.arn}/*",
-          aws_s3_bucket.replica_bucket.arn,
-          "${aws_s3_bucket.replica_bucket.arn}/*"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Create Primary Lambda Function
-resource "aws_lambda_function" "primary_lambda" {
-  function_name = "primary_lambda"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "primary_lambda.lambda_handler"
-
-  filename = "${path.module}/lambda/primary_lambda.py.zip"
-
-  environment {
-    variables = {
-      BUCKET_NAME = aws_s3_bucket.primary_bucket.id
-    }
-  }
-}
-
-# Create Backup Lambda Function
-resource "aws_lambda_function" "backup_lambda" {
-  function_name = "backup_lambda"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "backup_lambda.lambda_handler"
-
-  filename = "${path.module}/lambda/backup_lambda.py.zip"
-
-  environment {
-    variables = {
-      BUCKET_NAME = aws_s3_bucket.replica_bucket.id
-    }
-  }
-}
-
-# S3 Event Notification for Primary Lambda
-resource "aws_s3_bucket_notification" "primary_bucket_notification" {
-  bucket = aws_s3_bucket.primary_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.primary_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-}
-
-# S3 Event Notification for Backup Lambda
-resource "aws_s3_bucket_notification" "replica_bucket_notification" {
-  bucket = aws_s3_bucket.replica_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.backup_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
 }
